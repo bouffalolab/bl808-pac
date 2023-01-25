9128d3ce5cc684ac8a16a188b7613f9c86aa6c6f
luojia65 <me@luojia.cc>
2023-01-25 22:46:37 +0800

    crate: use svd2rust 0.28.0 command line params

--- generate.ps1
+++ generate.ps1
@@ -1,4 +1,4 @@
-svd2rust -i bl808.svd --target riscv --const_generic --keep_list --nightly
+svd2rust -i bl808.svd --target riscv --const_generic --keep_list --atomics
 Remove-Item -Path "src" -Recurse
 form -i lib.rs -o src/ 
 Remove-Item lib.rs
--- generate.sh
+++ generate.sh
@@ -1,4 +1,4 @@
-svd2rust -i bl808.svd --target riscv --const_generic --keep_list --nightly
+svd2rust -i bl808.svd --target riscv --const_generic --keep_list --atomics
 rm -rf src
 form -i lib.rs -o src/ 
 rm lib.rs
